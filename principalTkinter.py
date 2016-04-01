0a15e7dc7c914d791b05539a7691f82a04c55d28
Gabriel Azevedo <gazevedo@outlook.com>
2016-04-01 01:26:21 -0300

    Tkinter
    - Mudança do nome da coluna id para matrícula;
    - Para adicionar, deletar e mudar nota de aluno, deve-se informar a
    matrícula;
    - Ao deletar, listbox não atualiza;
    - Mudança de nota funcional, porém a listbox não atualiza. Deve-se
    fechar e abrir;
    - Exportar continua desativado;

--- principalTkinter.py
+++ principalTkinter.py
@@ -9,7 +9,7 @@ cursor = conn.cursor()
 def criarTabela():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS alunos (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            matricula INTEGER NOT NULL PRIMARY KEY,
             nome TEXT NOT NULL,
             nota TEXT NOT NULL
         );
@@ -20,31 +20,31 @@ criarTabela()
 #### Definições da Aplicação Principal ###
 principal = Tk()
 principal.title("Alumn - Principal")
-principal.geometry("600x281")
+principal.geometry("600x333")
 principal.resizable(FALSE, FALSE)
 
 #### Funções ###
 def adicionar_estudante():
+    matricula = etMatricula.get()
     nome = etNome.get()
     nota = etNota.get()
     cursor.execute("""
-        INSERT INTO alunos (nome, nota) VALUES (?, ?)""", (nome, nota))
+        INSERT INTO alunos (matricula, nome, nota) VALUES (?, ?, ?)""", (matricula, nome, nota))
     conn.commit()
-    lstAlunos.insert(END, (nome, nota))
+    lstAlunos.insert(END, (matricula, nome, nota))
 
 def deletar_estudante():
-    id_aluno = etID.get()
-    print(delALuno)
+    matricula_aluno = etMatriculaDeletar.get()
     cursor.execute("""
-        DELETE FROM alunos WHERE id=?""", (id_aluno,))
+        DELETE FROM alunos WHERE matricula=?""", (matricula_aluno,))
     conn.commit()
-    lstAlunos.delete(ANCHOR)
+    lstAlunos.delete(matricula_aluno)
 
 def mudar_nota():
+    matricula_aluno = etMatriculaMudar.get()
     nova_nota = etNovaNota.get()
-    mudar_nota = str(lstAlunos.get(ACTIVE))
     cursor.execute("""
-        UPDATE alunos SET nota = ? WHERE id = ?""", (nova_nota, mudar_nota))
+        UPDATE alunos SET nota = ? WHERE matricula = ?""", (nova_nota, matricula_aluno))
     conn.commit()
 
 def exportar():
@@ -61,31 +61,35 @@ def exportar():
 
 ### Widgets - Principal ###
 lblTitulo = Label(principal, text="Alumn")
-lblNomeNota = Label(principal, text="ID / Nome / Nota")
+lblNomeNota = Label(principal, text="Matrícula / Nome / Nota")
 
 ### Widgets - Adicionar Aluno ###
 lblAdicionarAluno = Label(principal, text="Adicionar Aluno")
+lblMatricula = Label(principal, text="Matrícula: ")
 lblNome = Label(principal, text="Nome do Aluno: ")
 lblNota = Label(principal, text="Nota do Aluno: ")
+etMatricula = Entry(principal)
 etNome = Entry(principal)
 etNota = Entry(principal)
 btnAdd = Button(principal, text="Adicionar", command=adicionar_estudante)
 
 ### Widgets - Deletar Aluno ###
 lblDeletarAluno = Label(principal, text="Deletar Aluno")
-lblID = Label(principal, text="ID: ")
-etID = Entry(principal, width=10)
+lblMatriculaDeletar = Label(principal, text="Matrícula: ")
+etMatriculaDeletar = Entry(principal, width=10)
 btnDel = Button(principal, text="Deletar", command=deletar_estudante)
 
 ### Widgets - Mudar Nota ###
 lblMudarNota = Label(principal, text="Mudar Nota")
+lblMatriculaMudar = Label(principal, text="Matrícula: ")
 lblNovaNota = Label(principal, text="Nota Nota: ")
+etMatriculaMudar = Entry(principal)
 etNovaNota = Entry(principal)
 btnMudarNota = Button(principal, text="Mudar Nota", command=mudar_nota)
 
 ### Widgets - Listar Alunos ###
 scrollbar = Scrollbar(principal)
-lstAlunos = Listbox(principal, width=35, height=13)
+lstAlunos = Listbox(principal, width=35, height=16)
 lstAlunos.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=lstAlunos.yview)
 lista = cursor.execute("""
@@ -104,22 +108,26 @@ scrollbar.place()
 
 ### Posicionamento de Widgets - Adicionar Aluno ###
 lblAdicionarAluno.place(x=100, y=30)
-lblNome.place(x=10, y=52)
-etNome.place(x=115, y=50)
-lblNota.place(x=10, y=82)
-etNota.place(x=115, y=80)
-btnAdd.place(x=115, y=115)
+lblMatricula.place(x=10, y=52)
+etMatricula.place(x=115, y=50)
+lblNome.place(x=10, y=82)
+etNome.place(x=115, y=80)
+lblNota.place(x=10, y=112)
+etNota.place(x=115, y=110)
+btnAdd.place(x=115, y=145)
 
 ### Posicionamento de Widgets - Deletar Aluno ###
-lblDeletarAluno.place(x=100, y=145)
-lblID.place(x=10, y=167)
-etID.place(x=50, y=165)
-btnDel.place(x=145, y=168)
+lblDeletarAluno.place(x=100, y=175)
+lblMatriculaDeletar.place(x=10, y=197)
+etMatriculaDeletar.place(x=80, y=195)
+btnDel.place(x=175, y=198)
 
 ### Posicionamento de Widgets - Mudar Nota ###
-lblMudarNota.place(x=100, y=195)
-lblNovaNota.place(x=10, y=217)
-etNovaNota.place(x=115, y=215)
-btnMudarNota.place(x=115, y=250)
+lblMudarNota.place(x=105, y=225)
+lblMatriculaMudar.place(x=10, y=247)
+etMatriculaMudar.place(x=115, y=245)
+lblNovaNota.place(x=10, y=277)
+etNovaNota.place(x=115, y=275)
+btnMudarNota.place(x=115, y=308)
 
 principal.mainloop()
